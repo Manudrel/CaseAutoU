e01ab3ee51841a452d80e9673b76f590a2a8c8f9
Manudrel <emanuelsimoesalmeida@gmail.com>
2025-11-13 22:34:02 -0300

    added forms.py, new model class called EmailMessage and the base.html was changed

---
+++ smart_reply/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import EmailMessage
+
+class EmailMessageForm(forms.ModelForm):
+    class Meta:
+        model = EmailMessage
+        fields = ['file', 'text']
+
+
--- smart_reply/models.py
+++ smart_reply/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+class EmailMessage(models.Model):
+
+    text = models.TextField(blank=True, null=True)
+    
+    file = models.FileField(
+        upload_to= 'uploads/',
+        blank=True,    
+        null=True,
+        validators=[FileExtensionValidator(allowed_extensions=['pdf', 'txt'])]
+    )
+    
+    def __str__(self):
+        return super().__str__()
